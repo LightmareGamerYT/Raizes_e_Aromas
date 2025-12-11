b1ebbf04bfb05154b7c7fee1986aa2adbe0b8471
MATHEUS DOS REIS SILVA <41607903806@imaccommand012.local>
2025-12-11 15:02:42 -0300

    Concluido

--- script.js
+++ script.js
@@ -1,48 +1,45 @@
-//Menu Hamburguer
-
+//Menu de Hamburguer
 let menu = document.getElementById("menu")
 let iconeBarras = document.getElementById("icone-barras")
 let iconeX = document.getElementById("icone-x")
+let body = document.getElementsByTagName("body")
 let sombra = document.getElementById("sombra")
 
+
 function abreFechaMenu() {
-    //Se o menu esta fechado
+    //Se o menu está fechado
     if (window.getComputedStyle(menu).right == "-210px") {
-        //Abrir o Menu
+        //Abrir menu
         menu.style.right = "0"
-
-        // Mostrar o Icone X
-
+        
+        //Mostrar icone x
         iconeX.style.display = "inline"
-
-        //esconder o icone de barras
-
+        
+        //Esconder icone barras 
         iconeBarras.style.display = "none"
-
+        
         sombra.style.right = "0"
-
-    }
-
-    else {
-        //Fechar o Menu
+    } else {
+        //Fechar o menu
         menu.style.right = "-210px"
-
-        //Esconder o Icone X
+        
+        // Esconder icone X
         iconeX.style.display = "none"
-
-        //Mostrar o icones barras
+        
+        // Mostrar icone barras
         iconeBarras.style.display = "inline"
-
+        
         sombra.style.right = "-101vw"
     }
-
 }
 
 onresize = () => {
     if (window.getComputedStyle(menu).right == "-210px") {
-        iconeX.style.display = "none"
+        // Mostrar icone X
+        iconeX.style.display = "inline"
     }
     else{
-        iconeBarras.style.display = "inline"
+        // Esconder icone barras
+        iconeBarras.style.display = "none"
     }
 }
